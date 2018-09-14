228a15266eb41ab3c54bcd1e27f7aeb9f2acf2f0
William Murray <willmurray461@gmail.com>
2018-09-14 09:49:09 -0400

    Fixed

--- HW 9 14 18/3.py
+++ HW 9 14 18/3.py
@@ -2,5 +2,8 @@ import sys
 m = int(sys.argv[1])
 d = int(sys.argv[2])
 y = int(sys.argv[3])
-answer = (d +(13*(m+1)/5) + (y%100) + (y/4) + (y/100/4) - 2*(y/100))%7
-print(str(answer))
+y0 = y - (14 - m)//12
+x = y0 + y0/4 - y0/100 + y0/400
+m0 = m + 12 * ((14 - m)//12) - 2
+d0 = (d + x + (31 * m0)//12)%7
+print(int(d0))
