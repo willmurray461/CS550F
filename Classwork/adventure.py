d4c31869073f0e46495569a1afb2a560fcc73dc8
William Murray <willmurray461@gmail.com>
2018-09-23 22:27:51 -0400

    updated code

--- Classwork/adventure.py
+++ Classwork/adventure.py
@@ -51,8 +51,8 @@ def start():
 				else:
 					sys.stdout.write("\033[F")
 					sys.stdout.write("\033[F")
-					print("Type either 'a,' 'b,' or 'c' to choose an option.")
-					a = str.lower(input(("User:\\> ")))
+					start = str.lower(input("Type either 'a,' 'b,' or 'c' to choose an option.\nUser:\\> "))
+					# start = str.lower(input(("User:\\> ")))
 		elif startanswer == "quit":
 			exit()
 		else:
@@ -60,10 +60,25 @@ def start():
 			sys.stdout.write("\033[F")
 			startanswer = input("Type 'continue' to continue or 'quit' to exit the program.\nUser:\\> ")
 def starta():
-	astart = str.lower(input("\tYou arrive at the nearest airport which is still 20 miles from where\nthe maptells you to go. You decide that you should try to take a bus\nto yourdestinaiton, but since you don't speak spanish, you take the wrong\nbus andaccidentally get lost. Fortunately, the bus you accidentally take leads\nyou to apopular tourist site where you find a hotel where people speak english."))
+	astart = str.lower(input("\tYou arrive at the nearest airport which is still 20 miles from where\nthe maptells you to go. You decide that you should try to take a bus\nto yourdestinaiton, but since you don't speak spanish, you take the wrong\nbus andaccidentally get lost. Fortunately, the bus you accidentally take leads\nyou to apopular tourist site where you find a hotel in which people speak\nenglish.The man at the front desk tells you which public bus to take, but says\nyou'll still need to walk a couple miles from the bus stop to get to your\ndestination. Another man says he knows a better way into the part of the forest\nyou need to go to, but he looks pretty sketchy. Do you want to:\nA: Take the bus the hotel guy told you to take.\nB: Use the sketchy guy's route.\n\nUser:\\>"))
+	validastart = False
+	while validastart == False:
+		if astart == "a":
+			validastart = True
+			print("Good call...\n")
+			ime.sleep(1.0)
+			startb()
+		if astart == "b":
+			validastart = True
+			print("Hmmm...OK...\n")
+			time.sleep(1.0)
+			ab()
+		else:
+			sys.stdout.write("\033[F")
+			sys.stdout.write("\033[F")
+			astart = str.lower(input("Type 'a' or 'b' to choose an option.\nUser:\\> "))
 def startb():
-	validbstartanswer = False
-	print("\tSince you were brave enough to take a bus in, you did some research on\nall the busses you needed to take and got to your destination. It's in the\nmiddle of nowhere and the map says as follows:")
+	print("\tAfter taking your busse(s), you got to your destination. At least, you\n got close to your destination. You still need to walk to the specified location on the map. The map says as follows:")
 	print("╔══════════════════════════════════════════════════════════════════════╗")
 	print("║         ^                  Treasure Map:                             ║")
 	print("║       ^^^^^    <–––––––––– Mountain                                  ║")
@@ -86,18 +101,135 @@ def startb():
 	print("╚══════════════════════════════════════════════════════════════════════╝")
 	time.sleep(7.0)
 	bstart = str.lower(input("\nWould you like to:\nA: Go through the forest.\nB: Take the dirt path around.\n\nUser:\\> "))
-	while validbstartanswer == False:
+	validbstart = False
+	while validbstart == False:
 		if bstart == "a":
+			validbstart = True
+			print("I see you're the adventurous type!\n")
+			time.sleep(1.0)
 			ba()
 		if bstart == "b":
+			validbstart = True
+			print("I see you're the careful type!\n")
+			time.sleep(1.0)
 			bb()
 		else:
 			sys.stdout.write("\033[F")
 			sys.stdout.write("\033[F")
-			bstart = input("Type either 'a,' 'b,' or 'c' to choose an option.\nUser:\\> ")
+			bstart = str.lower(input("Type either 'a,' 'b,' or 'c' to choose an option.\nUser:\\> "))
 def startc():
-	print("")
-startb()
+	cstart = str.lower(input("\tSo you give up on trying to find the treasure and decide to put the map\naway. I guess treasure hunting just isn't for you.\nTHE END.\nWow, you're really boring...\nWant to play again? [Y/N]\n\nUser:\\> "))
+	validcstart = False
+	while validcstart == False:
+		if cstart == "y":
+			validcstart = True
+			print("Nice to see you again...\n")
+			time.sleep(1.0)
+			start()
+		if cstart == "n":
+			validcstart = True
+			print("Goodbye!\n")
+			time.sleep(1.0)
+			exit()
+		else:
+			sys.stdout.write("\033[F")
+			sys.stdout.write("\033[F")
+			cstart = str.lower(input("To choose an option, type 'y' or 'n.'\nUser:\\>"))
+def ab():
+	abstart = str.lower(input("\tYou take the sketchy guy's bus route and it actually works. You get to\nexactly where you need to be. However, he demands you pay him 50 times his\nadvertised price. You tell him that you don't have that amount of money and he\ngives you an option:\nA: You deliver a mysterious package to 'a friend' back in town.\nYou can also:\nB: You offer to trade him a picture of a (your) treasure map via email as well\nas pay him the advertised price, but only if he waits until tomorrow\nC: Make a run for it and hide in the forest.\nWhich do you choose?\n\nUser:\\>"))
+	validabstart = False
+	while validabstart == False:
+		if abstart == "a":
+			validabstart = True
+			print("OK...\n")
+			time.sleep(1.0)
+			aba()
+		if abstart == "b":
+			validabstart = True
+			print("Interesting idea...\n")
+			time.sleep(1.0)
+			abb()
+		if abstart == "c":
+			validabstart = True
+			print("I hope you're fast!\n")
+			time.sleep(1.0)
+			abc()
+		else:
+			sys.stdout.write("\033[F")
+			sys.stdout.write("\033[F")
+			cstart = str.lower(input("Type either 'a,' 'b,' or 'c' to choose an option.\nUser:\\> "))
+def aba():
+	abastart = str.lower(input("\tYou decide to go deliver the package first in exchange for him driving\nyou to the treasure site. He gives you a package and a drop off location and\nsends you on your way. You get to the dropoff point, knock on the door, but just\nas it opens, several undercover policemen detain you for questioning. They\neventually open the package and find a bomb inside and arrest you.\nYour options are:\nA: You tell them your story and ask them for freedom in exchange for leading them\nto the person whogave you the package.\nB: Bribe one of the police guards with the treasure map\n(you have a picture of it saved anyway)\nC: Try to pick the lock while the guard is not looking\n\nUser:\\>"))
+	validabastart = False
+	while validabastart == False:
+		if abastart == "a":
+			validabastart = True
+			print("Good idea...\n")
+			time.sleep(1.0)
+			abaa()
+		if abastart == "b":
+			validabastart = True
+			print("I hope the gaurds here are corrput...\n")
+			time.sleep(1.0)
+			abab()
+		if abastart == "b":
+			validabastart = True
+			print("Better not get caught!\n")
+			time.sleep(1.0)
+			abab()
+		else:
+			sys.stdout.write("\033[F")
+			sys.stdout.write("\033[F")
+			abastart = str.lower(input("To choose an option, type 'y' or 'n.'\nUser:\\>"))
+def abb():
+	print("\tHe agrees, but warns you that if you don't show up the next day,\nhe will find you and kill you. You find the treasure and run away.\nYou become super rich but live the rest of your life in fear that he will kill you.\nTHE END")
+	exit()
+def abc():
+	abcstart = str.lower(input("\tYou try to run away but one of the guards shoots you and you die.\nTHE END\nWant to play again? [Y/N]\n\nUser:\\> "))
+	validabcstart = False
+	while validabcstart == False:
+		if abcstart == "y":
+			validabcstart = True
+			print("Nice to see you again...\n")
+			time.sleep(1.0)
+			start()
+		if abcstart == "n":
+			validabcstart = True
+			print("Goodbye!\n")
+			time.sleep(1.0)
+			exit()
+		else:
+			sys.stdout.write("\033[F")
+			sys.stdout.write("\033[F")
+			abcstart = str.lower(input("To choose an option, type 'y' or 'n.'\nUser:\\>"))
+def abaa():
+	print("\tYou lead the police to the shady man when you return the next day and\nthey agree to release you. You then find the treasure.\nTHE END")
+	exit()
+def abab():
+	ababstart = str.lower(input("\tSome guards see you and send you to prison. Bad mistake.\nTHE END\nWant to play again?"))
+	validababstart = False
+	while validababstart == False:
+		if ababstart == "y":
+			validababstart = True
+			print("Nice to see you again...\n")
+			time.sleep(1.0)
+			start()
+		if ababstart == "n":
+			validababstart = True
+			print("Goodbye!\n")
+			time.sleep(1.0)
+			exit()
+		else:
+			sys.stdout.write("\033[F")
+			sys.stdout.write("\033[F")
+			ababstart = str.lower(input("To choose an option, type 'y' or 'n.'\nUser:\\>"))
+def ba():
+	bastart = print("\tYou get lost in the jungle and die.\nTHE END.")
+	exit()
+def bb():
+	bastart = print("\tYou follow the path to the treasure and become rich.\nTHE END.")
+	exit()
+start()
 
 
 
