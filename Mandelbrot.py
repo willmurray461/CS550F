718c9f6e68046ae9c47a1205db551ed4a113a6a9
William Murray <willmurray461@gmail.com>
2018-10-18 21:21:08 -0400

    Finished Mandelbrot

---
+++ Mandelbrot.py
@@ -0,0 +1,67 @@
+#importing necessary libraries for math calculations and image generation
+import math
+from PIL import Image
+
+#defines a function to generate a mandelbrot image
+def mandelbrot(sizex, sizey, accuracy):
+	#initializes a new blank RGB image with a resolution of sizex x sizey
+	mandelbrot = Image.new("RGB", (sizex, sizey))
+	#creates a one dimentional array which will hold the values for each pixel
+	coords = [[] for x in range(0, sizey)]
+	#adds another dimention to the aforementioned array, thus creating a 2D array of pixels
+	for x in range(0, sizey):
+		coords[x] = [[] for y in range(0, sizex)]
+	#This function loops for each x,y value in the array and determines which shade of red each pixel will be
+	for x in range(0, sizey):
+		for y in range(0, sizex):
+			#This variable represents the x component value of C
+			cx = -2 + ((4/(sizey - 1))*x)
+			#This variable represents the y component value of C
+			cy = -2 + ((4/(sizex - 1))*y)
+			#This represents the first (not zeroeth) x component value of Z
+			zx = -2 + ((4/(sizey - 1))*x)
+			#This represents the first (not zeroeth) y component value of Z
+			zy = -2 + ((4/(sizex - 1))*y)
+			#This is a temporary variable used for updating the value of zx and zy
+			tempvar = 0
+			#This is a variable to determine whether or not Z has escaped
+			escaped = False
+			#This is a counter to measure how many times the following while loop has run
+			counter = 0
+			#This while loop checks how many times it takes Z to escape, or if it doesn't after 255 attempts
+			#Accuracy is how many shades of red the user wants to use 0-256
+			while escaped == False and counter < accuracy:
+				#This checks if Z has escaped
+				if math.sqrt(zx**2 + zy**2) >= 2: 
+					#This sets the shade of red of the pixel x,y depending on how many attempts it took to escape and the level of accuracy
+					coords[x][y] = int(counter/accuracy * 255)
+					#This tells the loop that the Z has escaped
+					escaped = True
+				#If Z has not escaped:
+				else:
+					#This uses 'tempvar' to store the value of zx
+					tempvar = zx
+					#This squares Z and adds C
+					zx = zx**2 - zy**2 + cx
+					zy = 2*tempvar*zy + cy
+					#This increases the counter by 1
+					counter += 1 
+				#If Z has not escaped after 255 attempts:
+				if counter == accuracy:
+					#This sets the shade of red of the pixel x,y to solid red
+					coords[x][y] = int(counter/accuracy * 255)
+
+	#This function loops over for each pixel x,y
+	for x in range(0, sizey):
+		for y in range(0, sizex):
+			#Converts the color value of each pixel in the pixel array to an integer
+			color = str(coords[x][y])
+			color = int(color)
+			#Places the pixel on the image
+			mandelbrot.putpixel((x, y), (color, 0, 0))
+
+	#Saves the image
+	mandelbrot.save("demo_image.png", "PNG")
+
+#Calls the Mandelbrot function
+mandelbrot(200,200,64)
--- pillow.py
+++ pillow.py
@@ -1,7 +1,8 @@
 from PIL import Image
 import math
+import random
 
-image = Image.new("RGB",(512, 512))
+image1 = Image.new("RGB",(512, 512))
 
 angle = 0
 radius = 0
@@ -9,6 +10,105 @@ radius = 0
 for x in range(0, 2560):
 	angle += 1
 	radius += 0.1
-	image.putpixel((math.floor(256 + radius*math.cos(math.radians(angle))), math.floor(256 + radius*math.sin(math.radians(angle)))), (255, 0, 0))
+	image1.putpixel((math.floor(256 + radius*math.cos(math.radians(angle))), math.floor(256 + radius*math.sin(math.radians(angle)))), (255, 0, 0))
 
-image.save("demo_image.png", "PNG")
+image2 = Image.new("RGB",(512, 512))
+
+column = 0
+
+row = 0
+
+for z in range(0, 8):
+	for x in range(0, 4):
+		for x in range (0, 64):
+			for y in range(0, 64):
+				image2.putpixel((x + column, y + row), (255, 0, 0))
+		column += 128
+	row += 64
+	if z%2 == 0:
+		column = 64
+	else:
+		column = 0
+
+image3 = Image.new("RGB", (512, 512))
+
+for z in range(0,50):
+	colorR = random.randrange(0,256)
+	colorG = random.randrange(0,256)
+	colorB = random.randrange(0,256)
+	x = random.randrange(0,512)
+	for y in range(0,512):
+		image3.putpixel((x, y), (colorR, colorG, colorB))
+		x += random.randrange(-3,4)
+		if x >= 512:
+			x = 511
+		elif x < 0:
+			x = 0
+
+image4 = Image.new("RGB", (512, 512))
+
+for z in range(0,50):
+	colorR = random.randrange(0,256)
+	colorG = random.randrange(0,256)
+	colorB = random.randrange(0,256)
+	x = random.randrange(0,512)
+	for y in range(0,512):
+		r = random.randrange(-3,4)
+		for w in range(0, abs(r) + 1):
+			if x + r >= 512:
+				r = 511 - x
+			elif x + r < 0:
+				r = 0 - x
+			if r > 0:
+				image4.putpixel((x + r - w, y), (colorR, colorG, colorB))
+			elif r < 0:
+				image4.putpixel((x + r + w, y), (colorR, colorG, colorB))
+			else:
+				image4.putpixel((x, y), (colorR, colorG, colorB))
+		x = x + r
+		
+def mandelbrot(sizex, sizey):
+	mandelbrot = Image.new("RGB", (sizex, sizey))
+
+	coords = [[] for x in range(0, sizey)]
+
+	for x in range(0, sizey):
+		coords[x] = [[] for y in range(0, sizex)]
+
+	for x in range(0, sizey):
+		for y in range(0, sizex):
+			cx = -2 + ((4/(sizey - 1))*x)
+			cy = -2 + ((4/(sizex - 1))*y)
+			coordx = -2 + ((4/(sizey - 1))*x)
+			coordy = -2 + ((4/(sizex - 1))*y)
+			tempvar = 0
+			escaped = False
+			counter = 0
+			while escaped == False and counter < 256:
+				if math.sqrt(coordx**2 + coordy**2) >= 2:
+					coords[x][y] = counter
+					escaped = True
+				else:
+					tempvar = coordx
+					coordx = coordx**2 - coordy**2 + cx
+					coordy = 2*tempvar*coordy + cy
+					counter += 1 
+				if counter == 64:
+					coords[x][y] = counter
+
+	for x in range(0, sizey):
+		for y in range(0, sizex):
+			color = str(coords[x][y])
+			color = int(color)
+			mandelbrot.putpixel((x, y), (color, 0, 0))
+
+	mandelbrot.save("demo_image.png", "PNG")
+
+mandelbrot(2000,2000)
+
+
+
+
+
+
+
